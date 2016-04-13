2f92d4ee7b1ed5512435e187b0a2150e8d261a27
Andreas Neumann <andneuma@posteo.de>
2016-04-13 15:17:37 +0200

    Change list structure of expenses in show view of expense_list

--- app/helpers/expense_lists_helper.rb
+++ app/helpers/expense_lists_helper.rb
@@ -1,20 +1,26 @@
-require 'Date'
+require 'date'
 
 module ExpenseListsHelper
   def namify_month(month)
+    # MONTHNAMES_GER = [nil, "Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"]
     Date::MONTHNAMES[month]
   end
 
-  def expenses_in_months
-    expenses_per_month = Hash[@expense_list.expenses.map {|expense| [expense[:when].month, []]}]
+  def expenses_in_year(expenses)
+    expenses.group_by {|e| e[:when].strftime("%y")}
+  end
 
-    @expense_list.expenses.each do |expense|
-      month_of_expense = expense[:when].month
-      expenses_per_month[month_of_expense] << expense
-      # debugger
-      expenses_per_month[month_of_expense].sort_by! {|e| e[:when]}
-    end
+  def expenses_in_months(expenses)
+    expenses.group_by {|e| e[:when].strftime("%m")}
 
-    expenses_per_month.sort
+    # expenses_per_month = Hash[expenses.map {|expense| [expense[:when].month, []]}]
+    #
+    # @expense_list.expenses.each do |expense|
+    #   month_of_expense = expense[:when].month
+    #   expenses_per_month[month_of_expense] << expense
+    #   expenses_per_month[month_of_expense].sort_by! {|e| e[:when]}
+    # end
+    #
+    # expenses_per_month.sort
   end
 end
