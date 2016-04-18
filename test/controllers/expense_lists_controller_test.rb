9ba2094c665097f12500ca9161afee5cfe59d353
Andreas Neumann <andneuma@posteo.de>
2016-04-18 17:56:52 +0200

    Finish Unit tests for expenses and expense_lists CRUD methods

--- test/controllers/expense_lists_controller_test.rb
+++ test/controllers/expense_lists_controller_test.rb
@@ -1,5 +1,63 @@
 require 'test_helper'
 
 class ExpenseListsControllerTest < ActionController::TestCase
-  
+
+# GET => 'get' (index, show)
+# POST => 'post' (new)
+# PUT => 'put' (update)
+
+  def setup
+    @expense_list = expense_lists :Essensliste
+  end
+
+  # INDEX
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  # SHOW
+  test "should get show" do
+    get :show, id: @expense_list.id
+    assert_response :success
+  end
+
+  # UPDATE
+  test "should get edit" do
+    get :edit, id: @expense_list.id
+    assert_response :success
+  end
+
+  test "should update valid changes" do
+    put :update, id: @expense_list.id, expense_list: {name: "Blubber", description: "lalala"}
+    assert_response :success
+    assert_template 'expense_lists/show'
+  end
+
+  test "should not update invalid changes to given expense list" do
+    put :update, id: @expense_list.id, expense_list: {name: "", description: "andere Beschreibung"}
+    @expense_list.reload.description
+    assert "Hier nehmen wir alle Essenseinkaufe auf" == @expense_list.description
+  end
+
+  # CREATE
+  test "should add new valid expense list" do
+    assert_difference 'ExpenseList.count', +1 do
+      post :create, expense_list: {name: "Blubber", description: "lalala", budget_in_euro: 23}
+    end
+  end
+
+  test "should not add invalid expense list" do
+    assert_no_difference 'ExpenseList.count' do
+      post :create, expense_list: {description: "lalala", budget_in_euro: 23}
+    end
+  end
+
+  # DESTROY
+  test "should delete" do
+    assert_difference 'ExpenseList.count', -1 do
+      delete :destroy, id:@expense_list.id
+    end
+    assert_redirected_to expense_lists_path
+  end
 end
--- test/controllers/expenses_controller_test.rb
+++ test/controllers/expenses_controller_test.rb
@@ -5,11 +5,58 @@ class ExpensesControllerTest < ActionController::TestCase
   #   assert true
   # end
 
-  # def setup
-  #   @expense = Expense.new(where: "Lidl", comment: "", when: Time.now, expenses_in_euro: 34.20)
+  def setup
+    @expense_list = expense_lists :Essensliste
+    @expense = expenses :expensive
+  end
+
+  # # TODO: Edit routing to redirect to 'expense_list#show'
+  # # bad fix => redirect in index controller
+  # test "should get index" do
+  #   get :index, expense_list_id: @expense_list.id
+  #   assert_response :missing
   # end
-  #
-  # test "index" do
-  #   get :index
-  # end
+
+  # UPDATE
+  test "should get edit for given expense" do
+    get :edit, expense_list_id: @expense_list.id, id: @expense.id
+    assert_response :success
+  end
+
+  test "should update valid changes to given expense" do
+    put :update, expense_list_id: @expense_list.id, id: @expense.id, expense: {where: "Aldi", when: "1954-04-23", expenses_in_euro: 23, comment: "adasdasd"}
+    @expense.reload.where
+    assert_equal "Aldi", @expense.where
+    assert_redirected_to expense_list_path(@expense_list)
+  end
+
+  test "should not update invalid changes to given expense" do
+    put :update, expense_list_id: @expense_list.id, id: @expense.id, expense: {when: "1954-04-23", expenses_in_euro: 238, comment: "adasdasd"}
+    @expense.reload.where
+    assert "Kaisers" == @expense.where
+  end
+
+  # CREATE
+  test "should add valid new expense" do
+    assert_difference 'Expense.count' do
+      post :create, expense_list_id: @expense_list.id, expense: {where: "Aldi", when: "1954-04-23", expenses_in_euro: 23, comment: "adasdasd"}
+    end
+    assert_redirected_to expense_list_path(@expense_list)
+  end
+
+  test "should not add invalid new expense" do
+    assert_no_difference 'Expense.count' do
+      post :create, expense_list_id: @expense_list.id, expense: {where: "Aldi", expenses_in_euro: 23, comment: "adasdasd"}
+    end
+  end
+
+  # DESTROY
+  test "should delete given expense" do
+    assert_difference 'Expense.count', -1 do
+      delete :destroy, expense_list_id: @expense_list.id, id: @expense.id
+    end
+    assert_redirected_to expense_list_path(@expense_list)
+
+  end
+
 end
