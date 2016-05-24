f8126eb96ea04b8bb7ee098e730b54f6357d65c8
andneuma <andneuma@posteo.de>
2016-05-24 13:54:09 +0200

    Fix bug in index view: Show expenses per month per expense_list

--- app/helpers/expense_lists_helper.rb
+++ app/helpers/expense_lists_helper.rb
@@ -9,14 +9,18 @@ module ExpenseListsHelper
     expenses.group_by { |e| e[:when].month }.sort.map { |month, expense| [Date::MONTHNAMES[month], expense] }
   end
 
-  def expenses_in_current_month
+  def expenses_in_current_month(expense_list)
     current_month = Date.today.strftime('%m')
-    expenses_in_current_month = Expense.all.select { |e| e.when.strftime('%m') == current_month }
-    expenses_in_current_month.map(&:expenses_in_euro).reduce(&:+)
+    expenses = expense_list.expenses.select { |e| e.when.strftime('%m') == current_month }
+    expenses.map(&:expenses_in_euro).reduce(&:+) || 0
   end
 
   def euros_left_in_current_month(expense_list)
     budget = expense_list.budget_in_euro
-    budget - expenses_in_current_month if budget
+    budget - expenses_in_current_month(expense_list) if budget
+  end
+
+  def euros_left_in_month(expense_list, month)
+    budget = expense_list.budget_in_euro
   end
 end
