a3a54dbfd7e0ab1c468fa65f3bb351daf7ea772d
andneuma <andneuma@posteo.de>
2016-05-24 14:13:06 +0200

    Changes of container format in static pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:sessions][:password])
       flash[:success] = "Willkommen #{@user.name}!"
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to expense_lists_path
     else
       flash[:danger] = 'Username oder Passwort falsch'
       render :new
