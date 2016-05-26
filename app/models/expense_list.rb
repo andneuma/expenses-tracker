99d99d1528f6a3c1c6bed71d947d0cb4a303bafc
andneuma <andneuma@posteo.de>
2016-05-26 22:22:57 +0200

    Fix of small bug which occured during refactoring

--- app/models/expense_list.rb
+++ app/models/expense_list.rb
@@ -26,20 +26,17 @@ class ExpenseList < ActiveRecord::Base
   end
 
   # VIRTUAL ATTRIBUTES
+  def expenses_in_month(month, year)
+    expenses.select do |e|
+      e.when.strftime('%m').to_i == month.to_i && e.when.strftime('%Y').to_i == year.to_i
+    end
+  end
+
   def sum_of_exp_in_month(month, year)
-    expenses = expenses_in_month(self, month, year)
-    expenses.map(&:expenses_in_euro).reduce(&:+).to_i || 0
+    expenses_in_month(month, year).map(&:expenses_in_euro).reduce(&:+).to_i || 0
   end
 
   def euros_left_in_month(month, year)
-    budget = budget_in_euro
-    budget - sum_of_exp_in_month(month, year) if budget
-  end
-
-  def expenses_in_month(month, year)
-    expenses = expenses.select do |e|
-      e.when.strftime('%m').to_i == month.to_i && e.when.strftime('%Y').to_i == year.to_i
-    end
-    expenses
+    budget_in_euro - sum_of_exp_in_month(month, year) if budget_in_euro
   end
 end
