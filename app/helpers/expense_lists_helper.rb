60af4556c462fd95cbb3bdcdd16e361b0e0d528d
andneuma <andneuma@posteo.de>
2016-10-05 15:29:27 +0200

    Refactor expense grouping helper code

--- app/helpers/expense_lists_helper.rb
+++ app/helpers/expense_lists_helper.rb
@@ -2,10 +2,10 @@ require 'date'
 
 module ExpenseListsHelper
   def expenses_by_year(expenses)
-    expenses.group_by { |e| e[:expense_date].strftime('%Y') }.sort
+    expenses.group_by(&:year).sort
   end
 
   def expenses_by_months(expenses)
-    expenses.group_by { |e| e[:expense_date].month }.sort.map { |month, expense| [month, expense] }
+    expenses.group_by(&:month).sort
   end
 end
