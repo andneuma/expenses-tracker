81ed6fe0c5beb9d0cd597fb63d83f484dd1209dc
andneuma <andneuma@posteo.de>
2016-10-21 13:02:31 +0200

    Tests green

--- test/controllers/expense_lists_controller_test.rb
+++ test/controllers/expense_lists_controller_test.rb
@@ -27,7 +27,6 @@ class ExpenseListsControllerTest < ActionController::TestCase
   end
 
   test 'should update valid changes' do
-    debugger
     put :update, id: @expense_list.id, expense_list:
     {
       name: 'Blubber',
