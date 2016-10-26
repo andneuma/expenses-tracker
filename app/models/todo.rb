ddd4e4f360337320a04baa2bfcfe620507bbd710
andneuma <andneuma@posteo.de>
2016-10-26 17:57:58 +0200

    Setup data model and scaffolds for todos and todo_lists

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,5 @@
 class Todo < ActiveRecord::Base
-  belongs_to :todo_list
+  belongs_to :todo_list, dependent: :destroy
+  has_and_belongs_to_many :users, dependent: :nullify
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026154631) do
+ActiveRecord::Schema.define(version: 20161026155453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "text"
+    t.integer  "user_id"
+    t.integer  "todo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["todo_id"], name: "index_comments_on_todo_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "expense_lists", force: :cascade do |t|
     t.string   "name"
@@ -76,6 +87,8 @@ ActiveRecord::Schema.define(version: 20161026154631) do
     t.boolean  "is_admin",        default: false
   end
 
+  add_foreign_key "comments", "todos"
+  add_foreign_key "comments", "users"
   add_foreign_key "expenses", "expense_lists"
   add_foreign_key "expenses", "users"
   add_foreign_key "todos", "todo_lists"
